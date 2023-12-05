a7a0b503c95520403cc08eec647c242367d44b0b
Lucienzan <changwilliam826@gmail.com>
2023-12-05 23:16:52 +0630

    [develop] #updated code

--- src/Tuto1.java
+++ src/Tuto1.java
@@ -1,7 +1,17 @@
 public class Tuto1 {
     public static void main(String[] args)
     {
-        System.out.println("hello");
+        // printf
+        char myChar = '@';
+        boolean isShow = true;
+        String name = "Lucien";
+        int intNumber = 12;
+
+        // b for boolean, c for char, d for integer
+        System.out.printf("%b this is printf",isShow+ "\n");
+        System.out.printf("%s this is printf",name+"\n");
+        System.out.printf("%d this is printf",intNumber);
+        System.out.printf("%c this is printf",myChar);
 
     }
 }
