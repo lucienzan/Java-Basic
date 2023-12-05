bd06dac89e45ea05c91100ed1454ed97f93283f2
TheinZan <theinzan@metateammyanmar.com>
2023-12-05 09:54:27 +0630

    [develop] #printf learning

--- src/Tuto1.java
+++ src/Tuto1.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class Tuto1 {
     public static void main(String[] args)
     {
-        System.out.println("hello");
+    	Scanner scanner = new Scanner(System.in);
+    	
+        //Printf
+    	String greet = "This is daniel";
+    	double myMoney = 1000.34000;
+    	char sign = '$';
+    	int age = 23;
+    	
+    	// for string use s
+    	System.out.printf("%s",greet);
+    	scanner.nextLine();
 
+    	// for int use "d"
+    	System.out.printf("I am %d years old",age);
+    	scanner.nextLine();
+    	
+    	String message = String.format("I have %.2f %s", myMoney,sign);
+    	System.out.println(message);
+    	scanner.nextLine();
+    	
+    	System.out.printf("%014f",myMoney); // 0001000.340000
+    	scanner.nextLine();
+    	
+    	System.out.printf("%,.2f",myMoney); // 1,000.34
+    	scanner.close();
     }
 }
